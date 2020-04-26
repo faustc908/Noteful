948485e57c9042aeb291edf10c54e2a96e882985
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-04-26 16:41:30 -0400

    Update NoteListNav.js

--- noteful/src/NoteListNav/NoteListNav.js
+++ noteful/src/NoteListNav/NoteListNav.js
@@ -1,45 +1,43 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import CircleButton from '../CircleButton/CircleButton'
 import ApiContext from '../ApiContext'
-import { countNotesForFolder } from '../notes-helpers'
-import './NoteListNav.css'
+import { findNote, findFolder } from '../notes-helpers'
+import './NotePageNav.css'
 
-export default class NoteListNav extends React.Component {
+export default class NotePageNav extends React.Component {
+  static defaultProps = {
+    history: {
+      goBack: () => { }
+    },
+    match: {
+      params: {}
+    }
+  }
   static contextType = ApiContext;
 
   render() {
-    const { folders=[], notes=[] } = this.context
+    const { notes, folders, } = this.context
+    const { noteId } = this.props.match.params
+    const note = findNote(notes, noteId) || {}
+    const folder = findFolder(folders, note.folderId)
     return (
-      <div className='NoteListNav'>
-        <ul className='NoteListNav__list'>
-          {folders.map(folder =>
-            <li key={folder.id}>
-              <NavLink
-                className='NoteListNav__folder-link'
-                to={`/folder/${folder.id}`}
-              >
-                <span className='NoteListNav__num-notes'>
-                  {countNotesForFolder(notes, folder.id)}
-                </span>
-                {folder.name}
-              </NavLink>
-            </li>
-          )}
-        </ul>
-        <div className='NoteListNav__button-wrapper'>
-          <CircleButton
-            tag={Link}
-            to='/add-folder'
-            type='button'
-            className='NoteListNav__add-folder-button'
-          >
-            <FontAwesomeIcon icon='plus' />
-            <br />
-            Folder
-          </CircleButton>
-        </div>
+      <div className='NotePageNav'>
+        <CircleButton
+          tag='button'
+          role='link'
+          onClick={() => this.props.history.goBack()}
+          className='NotePageNav__back-button'
+        >
+          <FontAwesomeIcon icon='chevron-left' />
+          <br />
+          Back
+        </CircleButton>
+        {folder && (
+          <h3 className='NotePageNav__folder-name'>
+            {folder.name}
+          </h3>
+        )}
       </div>
     )
   }
